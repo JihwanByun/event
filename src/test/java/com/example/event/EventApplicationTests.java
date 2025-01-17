f1cbc190d0c33f2bda83befe5e5eebf05dc93d63
jihwan <jihwaja9091@naver.com>
2025-01-17 14:18:39 +0900

    Revert "feat: 명세서에 따른 테스트 코드 DisplayName 작성"
    This reverts commit 199a7ab8354213546d20c5d8f25ca5d5a2e593bd.

--- src/test/java/com/example/event/EventApplicationTests.java
+++ src/test/java/com/example/event/EventApplicationTests.java
@@ -20,25 +20,21 @@ class EventApplicationTests {
 	@Test
 	@DisplayName("사용자는 예약 확정된 티켓에 대해 이벤트 시작 24시간 전까지 취소가 가능합니다.")
 	void cancelTicket(){
-	}
 
-	@Test
-	@DisplayName("환불 규정에 따라 일주일 전 취소한 티켓의 경우 100%, 3일전의 경우 50% 환불이 가능하며, 당일 환불은 불가합니다. ")
-	void refundTicketPrice(){
 	}
 
 	/*
 		관리자 기능
 	 */
 	@Test
-	@DisplayName("관리자는 현재보다 3일 이상 남은 이벤트에 대해서만 새로운 이벤트를 생성할 수 있습니다.")
+	@DisplayName("관리자는 이벤트 생성, 수정, 삭제가 가능합니다.")
 	void createEvent(){
-	}
 
+	}
 	@Test
 	@DisplayName("관리자는 좌석 및 티켓 수를 정할 수 있습니다.")
 	void decideTotalTicket(){
-
+		
 	}
 
 
