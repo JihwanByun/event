fd8af7c27341defdc31359d613356de1470395c0
jihwan <jihwaja9091@naver.com>
2025-01-17 14:02:26 +0900

    Revert "Feat: sponsor domain column"
    This reverts commit f3df7d3246431db60a8a2ab45019767d008c65e4.

--- src/main/java/com/example/event/domain/Sponsor.java
+++ src/main/java/com/example/event/domain/Sponsor.java
@@ -1,9 +1,6 @@
 package com.example.event.domain;
 
-import java.time.LocalDateTime;
-
 public class Sponsor {
     private String name;
     private String donation;
-    private LocalDateTime startSponsorDate;
 }
