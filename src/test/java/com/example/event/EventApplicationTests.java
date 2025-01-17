3baf230ada2bfc606a717f477e8e169b8003fb12
jihwan <jihwaja9091@naver.com>
2025-01-17 14:34:49 +0900

    Feat: 테스트 display 작성

--- src/test/java/com/example/event/EventApplicationTests.java
+++ src/test/java/com/example/event/EventApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.event;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EventApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("사용자는 조건(이름,날짜,장소,가격,좌석 종류)에 따라 이벤트 목록을 검색할 수 있습니다.")
+	void searchEventByCondition() {
+	}
+
+	@Test
+	@DisplayName("사용자는 3분 이내에 판매 중인 티켓에 대해 결제할 수 있습니다.")
+	void buyTicket(){
+	}
+
+	@Test
+	@DisplayName("사용자는 공연 시작 1시간 전 예약 확정된 티켓에 대해 취소가 가능합니다.")
+	void cancelTicket(){
+	}
+
+	/*
+		관리자 기능
+	 */
+	@Test
+	@DisplayName("관리자는 현재보다 3일 이상 남은 이벤트에 대해서만 새로운 이벤트를 생성할 수 있습니다.")
+	void createEvent(){
+	}
+
+	@Test
+	@DisplayName("관리자는 좌석 및 티켓 수를 정할 수 있습니다.")
+	void decideTotalTicket(){
+
 	}
 
 }
