6df8742a0e110033af30d203a563a31f934995b6
jihwan <jihwaja9091@naver.com>
2025-01-19 21:45:51 +0900

    Feat: add event end date time validation test code

--- src/test/java/com/example/event/domain/EventTest.java
+++ src/test/java/com/example/event/domain/EventTest.java
@@ -5,6 +5,7 @@ import com.example.event.HostTestFixtures;
 import com.example.event.SponsorTestFixtures;
 import com.example.event.domain.value.Sponsor;
 import com.example.event.domain.value.Venue;
+import com.example.event.exception.event.EventCreateEndDateException;
 import com.example.event.exception.event.EventCreateTicketNegativeException;
 import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
@@ -12,6 +13,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class EventTest {
+
+    String EVENT_NAME = EventTestFixtures.EVENT_NAME;
+    Venue EVENT_VENUE = EventTestFixtures.EVENT_VENUE;
+    Sponsor SPONSOR = SponsorTestFixtures.createSponsor();
+    Host HOST = HostTestFixtures.createHost();
+    int TOTAL_TICKET_NUMBER = 10;
+    LocalDateTime EVENT_START_DATE_TIME = EventTestFixtures.startDateTime;
+    LocalDateTime EVENT_END_DATE_TIME = EventTestFixtures.endDateTime;
 
     @Test
     @DisplayName("판매 가능한 티켓은 0장 이하로 설정할 수 없습니다.")
@@ -21,14 +30,9 @@ class EventTest {
 
         //when & then
         Assertions.assertThatThrownBy(
-                () -> Event.createEvent(EventTestFixtures.EVENT_NAME,
-                    EventTestFixtures.EVENT_VENUE,
-                    HostTestFixtures.createHost(),
-                    SponsorTestFixtures.createSponsor(),
+                () -> Event.createEvent(EVENT_NAME, EVENT_VENUE, HOST, SPONSOR,
                     totalTicketNumber,
-                    EventTestFixtures.startDateTime,
-                    EventTestFixtures.endDateTime
-                ))
+                    EVENT_START_DATE_TIME, EVENT_END_DATE_TIME))
             .isInstanceOf(EventCreateTicketNegativeException.class)
             .hasMessage("Number of Ticket can't zero or negative");
     }
@@ -38,15 +42,19 @@ class EventTest {
     public void addEventDuration() {
 
         //given
+        LocalDateTime endEventDateTime = EVENT_START_DATE_TIME.minusDays(1);
 
-        //when
-
-        //then
-
+        //when & then
+        Assertions.assertThatThrownBy(
+                () -> Event.createEvent(EVENT_NAME, EVENT_VENUE, HOST, SPONSOR, TOTAL_TICKET_NUMBER,
+                    EVENT_START_DATE_TIME,
+                    endEventDateTime))
+            .isInstanceOf(EventCreateEndDateException.class)
+            .hasMessage("Event can be hold after open time");
     }
 
     @Test
-    @DisplayName("관리자는 현재 시간보다 3일 이상 남은 이벤트에 대해서만 새로운 이벤트를 생성할 수 있다.")
+    @DisplayName("관리자는 현재 시각보다 3일 이후에 열리는 이벤트만 생성할 수 있습니다. ")
     public void addEventStartDateTime() {
 
         //given
