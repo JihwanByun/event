973e56bb3ff65bae0e5b113453831d8a0343ef30
jihwan <jihwaja9091@naver.com>
2025-01-24 16:09:51 +0900

    Feat: 이메일, 핸드폰 객체 형식에 대한 입력값 메시지를 추가했습니다

--- src/main/java/com/example/event/exception/ErrorCode.java
+++ src/main/java/com/example/event/exception/ErrorCode.java
@@ -10,10 +10,10 @@ public enum ErrorCode {
     EVENT_ENDDATE_CREATE_ERROR(400, "Event can be hold after open time"),
 
     //Email
-    EMAIL_FORMAT_ERROR(400, "Follow the email format" ),
+    EMAIL_FORMAT_ERROR(400, "Follow the email format [@와 .을 포함한 이메일 형식을 지키세요]"),
 
     //Phone
-    PHONENUMBER_FORMAT_ERROR(400, "Follow the phone number format" );
+    PHONENUMBER_FORMAT_ERROR(400, "Follow the phone number format [연속된 숫자 11자리 형식을 지키세요]");
 
 
     @Getter
