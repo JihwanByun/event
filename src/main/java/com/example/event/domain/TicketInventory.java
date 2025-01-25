b6f7c6b307d5dedf7dca113292081659a57769ee
jihwan <jihwaja9091@naver.com>
2025-01-25 23:17:48 +0900

    Feat: Ticket, Event 도메인 테스트 코드 분리

--- src/main/java/com/example/event/domain/TicketInventory.java
+++ src/main/java/com/example/event/domain/TicketInventory.java
@@ -2,13 +2,19 @@ package com.example.event.domain;
 
 import com.example.event.domain.value.TicketType;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class TicketInventory {
 
-    private final Map<TicketType, List<Ticket>> tickets = new HashMap<>();
+    private final Map<TicketType, List<Ticket>> tickets;
+
+    private TicketInventory() {
+        this.tickets = new HashMap<>();
+    }
 
     public int getTotalTicketQuantity() {
         return tickets.values().stream().mapToInt(List::size).sum();
@@ -16,6 +22,17 @@ public class TicketInventory {
 
     public void addTickets(TicketType ticketType, int stock, int price,
         LocalDateTime releaseDateTime, LocalDateTime deadLineDateTime) {
+        this.tickets.putIfAbsent(ticketType, new ArrayList<>());
+        List<Ticket> newTickets = IntStream.range(0, stock)
+            .mapToObj(
+                value -> Ticket.createTicketNotReleased(ticketType, price, releaseDateTime,
+                    deadLineDateTime))
+            .toList();
 
+        this.tickets.put(ticketType, newTickets);
+    }
+
+    public static TicketInventory createTicketInventory() {
+        return new TicketInventory();
     }
 }
