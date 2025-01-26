7ecc9c3d87c4525a116fdce8b1eec259786bcaec
jihwan <jihwaja9091@naver.com>
2025-01-26 22:06:11 +0900

    Feat: 티켓 판매 개시, 마감 일정 설정 테스트 코드 작성

--- src/test/java/com/example/event/domain/TicketTest.java
+++ src/test/java/com/example/event/domain/TicketTest.java
@@ -3,7 +3,8 @@ package com.example.event.domain;
 import com.example.event.EventTestFixtures;
 import com.example.event.TicketTestFixtures;
 import com.example.event.domain.value.TicketType;
-import com.example.event.exception.event.TicektStockNegativeException;
+import com.example.event.exception.event.TicketStockNegativeException;
+import com.example.event.exception.ticket.TicketReleasedDateTimeException;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,8 +31,35 @@ public class TicketTest {
         assertThatThrownBy(
             () -> ticketInventory.addTickets(ticketType, ticketStock, ticketPrice, releasedDateTime,
                 deadLineDateTime))
-            .isInstanceOf(TicektStockNegativeException.class)
-            .hasMessage(TicektStockNegativeException.MESSAGE);
+            .isInstanceOf(TicketStockNegativeException.class)
+            .hasMessage(TicketStockNegativeException.MESSAGE);
+    }
+
+    @Test
+    @DisplayName("티켓은 이벤트 시작 30일 이전부터 10일전까지만 판매할 수 있다.")
+    void createTicketOnlyEventStartBefore30DayBetween10Day() {
+
+        //given
+        Event event = EventTestFixtures.createEvent();
+        TicketType ticketType = TicketTestFixtures.typeVIP;
+        int stock = TicketTestFixtures.stock;
+        int price = TicketTestFixtures.price;
+
+        //when
+        TicketInventory ticketInventory = TicketInventory.createTicketInventoryOfEvent(event);
+        LocalDateTime eventStartDateTime = event.getStartDateTime();
+        LocalDateTime ticketReleaseDateTime = event.getStartDateTime().minusDays(30);
+        LocalDateTime ticketDeadLineDateTime = event.getStartDateTime().minusDays(10);
+        ticketInventory.addTickets(ticketType, stock, price, ticketReleaseDateTime,
+            ticketDeadLineDateTime);
+
+        //then
+        assertThat(
+            ticketInventory.getTickets().get(ticketType).get(0).getReleaseDateTime()).isEqualTo(
+            ticketReleaseDateTime);
+        assertThat(
+            ticketInventory.getTickets().get(ticketType).get(0).getDeadLineDateTime()).isEqualTo(
+            ticketDeadLineDateTime);
     }
 
 
@@ -44,29 +72,20 @@ public class TicketTest {
     void canBuyTicketWhenTicketStatusForSale() {
     }
 
-    @Test
-    @DisplayName("티켓은 이벤트 시작 30일 이전부터 10일전까지만 판매를 개시할 수 있다.")
-    void createTicketOnlyEventStartBefore30DayBetween10Day() {
-
-    }
-
 
     @Test
     @DisplayName("티켓은 하나의 이벤트에 대해 사용자당 최대 2매를 초과할 수 없다.")
     void failToBuyTicketMoreThan2PerEvent() throws Exception {
-
     }
 
     @Test
     @DisplayName("이미 판매된 티켓은 구매할 수 없다.")
     void failToBuyTicketStatusIsSold() {
-
     }
 
     @Test
     @DisplayName("판매 전인 티켓은 구매할 수 없다.")
     void failToBuyTicketStatusIsNotReleased() {
-
     }
 
     /*
