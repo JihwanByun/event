c899be6a7893398fad282b3c763a90a371518853
jihwan <jihwaja9091@naver.com>
2025-02-01 18:51:23 +0900

    Feat: 공지사항 Optional 반환 구현

--- src/main/java/com/example/event/domain/Event.java
+++ src/main/java/com/example/event/domain/Event.java
@@ -9,6 +9,7 @@ import com.example.event.exception.event.EventCreateStartDateException;
 import com.example.event.exception.event.EventCreateTicketNegativeException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -35,8 +36,11 @@ public class Event {
     @Getter
     private final LocalDateTime endDateTime;
 
-    @Getter
     private final Announcement announcement; //선택 필드
+
+    public Optional<Announcement> getAnnouncementOfEvent(){
+        return Optional.ofNullable(announcement);
+    }
 
     private Event(String eventName, Venue venue, Host host, Sponsor sponsor, int totalTicketNumber,
         LocalDateTime startDateTime, LocalDateTime endDateTime, Announcement announcement) {
