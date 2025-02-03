1a9a056e4adf3beb7b0889a2245a09141df10e22
jihwan <jihwaja9091@naver.com>
2025-02-03 16:18:30 +0900

    feat: 구매, 환불 내역 객체 생성을 통한 거래내역 불변성 관리

--- src/main/java/com/example/event/domain/Ticket.java
+++ src/main/java/com/example/event/domain/Ticket.java
@@ -2,35 +2,30 @@ package com.example.event.domain;
 
 import com.example.event.domain.value.TicketType;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Ticket {
 
     private final int price;
-
     private TicketStatus status;
-
     private final TicketType type;
-
     private final LocalDateTime releaseDateTime;
-
     private final LocalDateTime deadLineDateTime;
-
-    private LocalDateTime payDateTime;
-
-    private LocalDateTime refundDateTime;
+    private final TransactionHistory transactionHistory;
 
     private Ticket(int price, TicketType type, LocalDateTime releaseDateTime,
-        LocalDateTime deadLineDateTime) {
+        LocalDateTime deadLineDateTime, TransactionHistory transactionHistory) {
         this.price = price;
         this.type = type;
         this.releaseDateTime = releaseDateTime;
         this.deadLineDateTime = deadLineDateTime;
+        this.transactionHistory = transactionHistory;
     }
 
     public static Ticket createTicket(int price, TicketType type, LocalDateTime releaseDateTime,
         LocalDateTime deadLineDateTime) {
 
-        return new Ticket(price, type, releaseDateTime, deadLineDateTime);
+        return new Ticket(price, type, releaseDateTime, deadLineDateTime, new TransactionHistory(new ArrayList<>()));
     }
 
     public void setTicketStatusForSale(Ticket ticket) {
