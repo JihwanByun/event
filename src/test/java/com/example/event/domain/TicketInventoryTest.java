1aecec54215194b3120005c6c781a17ae4a68102
jihwan <jihwaja9091@naver.com>
2025-02-06 14:17:25 +0900

    Feat: TicketInventory 생성 테스트 코드 작성

--- src/test/java/com/example/event/domain/TicketInventoryTest.java
+++ src/test/java/com/example/event/domain/TicketInventoryTest.java
@@ -1,5 +1,27 @@
 package com.example.event.domain;
 
+
+import com.example.event.EventTestFixtures;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class TicketInventoryTest {
+
+    @Test
+    @DisplayName("티켓 인벤토리 생성 성공")
+    public void createTicketInventorySuccessfully() {
+
+        //given
+        Event event = EventTestFixtures.createEvent();
+
+        //when
+        TicketInventory ticketInventory = TicketInventory.createTicketInventoryOfEvent(event);
+
+        //then
+        assertThat(ticketInventory).isNotNull();
+        assertThat(ticketInventory.getEvent()).isEqualTo(event);
+    }
 
 }
