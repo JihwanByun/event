8b5a6478458a94cabf3eb6d797a980a5288e07ee
jihwan <jihwaja9091@naver.com>
2025-02-24 18:50:35 +0900

    Feat: 웹 예외처리 메시지 추가

--- src/main/java/com/example/event/exception/ErrorCode.java
+++ src/main/java/com/example/event/exception/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
     TICKET_STOCK_ERROR(400, "Number of Ticket can't zero or negative"),
     TICKET_PRICE_ERROR(400, "Ticket Price can't be negative"),
     TICKET_RELEASED_DATETIME_ERROR(400, "티켓 판매는 이벤트 시작 이전 30일 부터 10일전까지만 가능합니다."),
-    TICKETTYPE_NOT_FOUND_ERROR(400, "해당 유형의 티켓을 찾을 수 없습니다. 티켓을 새로 등록하거나, 등록된 티켓을 입력하세요."),
+    TICKET_TYPE_NOT_FOUND_ERROR(400, "해당 유형의 티켓을 찾을 수 없습니다. 티켓을 새로 등록하거나, 등록된 티켓을 입력하세요."),
     TICKET_OUT_OF_STOCK_ERROR(400, "구매 수량이 티켓의 재고 수량보다 많습니다.");
 
 
