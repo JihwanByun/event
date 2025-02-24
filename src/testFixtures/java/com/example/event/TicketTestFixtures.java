87977e2e29e3fb4ebbdbf3c46dff2e9012f76921
jihwan <jihwaja9091@naver.com>
2025-02-24 18:51:23 +0900

    Feat: 티켓 추가 메서드 테스트 픽처스 추가

--- src/testFixtures/java/com/example/event/TicketTestFixtures.java
+++ src/testFixtures/java/com/example/event/TicketTestFixtures.java
@@ -1,26 +1,33 @@
 package com.example.event;
 
 import com.example.event.domain.Event;
+import com.example.event.domain.Ticket;
 import com.example.event.domain.TicketInventory;
 import com.example.event.domain.value.TicketType;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TicketTestFixtures {
 
     private static final int price = 100;
     private static final int stock = 100;
-    private static final TicketType typeVIP = new TicketType("VIP");
+    private static final TicketType VIP = TicketType.VIP;
     private static final Event event = EventTestFixtures.createEvent();
     private static final LocalDateTime releaseDateTime = event.getStartDateTime().minusDays(13);
     private static final LocalDateTime deadLineDateTime = releaseDateTime.plusDays(3);
 
-    public static TicketInventory createTicketInventoryOfEvent(
-        Event event
-    ) {
-        TicketInventory ticketInventory = TicketInventory.createTicketInventoryOfEvent(event);
-        ticketInventory.addTickets(typeVIP, stock, price, releaseDateTime, deadLineDateTime);
+    public static TicketInventory createTicketInventoryOfEvent(Event event) {
 
-        return ticketInventory;
+        return TicketInventory.createTicketInventoryOfEventWithSalesDuration(
+                event, releaseDateTime, deadLineDateTime
+        );
     }
 
+    public static List<Ticket> addTicketsWithTypeAndQuantity(TicketType ticketType, int quantity) {
+        return Stream.generate(() -> Ticket.createTicket(event, price, ticketType))
+                .limit(quantity)
+                .collect(Collectors.toList());
+    }
 }
